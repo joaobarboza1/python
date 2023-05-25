7a506a76b02a531dfef50d1cd43bc56ca6d1d97a
Santana <joaopedrosantanabarboza@gmail.com>
2023-05-25 20:54:39 -0300

    Novos exercicios adicionados

---
+++ exercicios.py
@@ -0,0 +1,27 @@
+# 1 - Faça um programa que solicite um número do usuário e apresente a seguinte mensagem na tela:
+# # # "O número digitado foi [número]"
+# numero=float(input('Preencha seu número: \n'))
+# print(f'O número digitado foi {numero}')
+# # # 2 - Escreva um programa que solicite o nome e o sobrenome do usuário. Ao final o programa deverá apresentar o nome completo do usuário na tela.
+# nome=input('Preencha seu nome')
+# sobrenome=input('Preencha seu  sobrenome')
+# print(f'Seu nome é {nome} {sobrenome}')
+# # # 3 - Faça um programa que solicite três números inteiros do usuário e imprima a soma destes.
+# num1=int(input('Escreva um numero: '))
+# num2=int(input('Escreva um numero: '))
+# num3=int(input('Escreva um numero: '))
+# print(f'A soma deste numeros é:{num1+num2+num3} ')
+# # # 4 - Escreva um programa que solicite duas notas do usuário e apresente a média na tela da seguinte forma:
+# # # "A média das notas [nota1] e [nota2] é [média]".
+# nota1=int(input('Escreva sua primeira nota: '))
+# nota2=int(input('Escreva sua segunda nota: '))
+# print(f'A média da suas notas é: {(nota1+nota2)/2}')
+
+# # # 5 - Escreva um programa que faça a conversão de um dado valor de metro para quilômetro. 
+
+# # # 6 - Escreva um programa que calcule a área de uma circunferência. O usuário deve digitar o valor do raio e ao final o programa deverá mostrar na tela a área da circunferência.
+# # # Use a fórmula: área=pi*r² , em que pi é uma constante e r o raio da circunferência.
+
+# # # 7 - Elabore um programa para calcular a hipotenusa de um triângulo.
+
+# # # 8 - Escreva um programa que peça um número inteiro do usuário e calcule e imprima a tabuada deste número.
